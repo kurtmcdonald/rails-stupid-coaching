2f0ca3a192c25f64a1c47ca7707afc60159e43fe
Kurt McDonald <kurtmcdonald24@gmail.com>
2021-08-17 11:18:04 +0100

    completed challenge

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,7 +6,7 @@ class QuestionsController < ApplicationController
     @question = params[:question]
     if @question.blank?
       @answer = "I can't hear you!"
-    elsif @question =~ /i am going to work/i
+    elsif @question == 'i am going to work'
       @answer = "Great!"
     elsif @question.ends_with?("?")
       @answer = "Silly question, get dressed and go to work!"
